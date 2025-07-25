ca556ee84efc9a95a3ff518cb643ab6292927468
Klaus Hildebrandt <klaus.hildebrandt@edeka.de>
2025-07-25 13:02:22 +0200

    add package refresh

---
+++ plugins/modules/package_refresh.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3.11
+"""
+Ansible Module to perform a package refresh on target host
+
+2025 Klaus Hildebrandt
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from __future__ import (absolute_import, division, print_function, annotations)
+__metaclass__ = type
+
+DOCUMENTATION = '''
+---
+module: dist_upgrade
+short_description: Perform channel exchange on host
+description:
+  - Perform channel change on host
+author:
+  - "Klaus Hildebrandt"
+'''
+import logging
+from ansible.module_utils.basic import AnsibleModule
+from datetime import datetime, timezone
+
+from ..module_utils.uyuni import UyuniAPIClient
+
+def main():
+    """
+    Main functions
+    """
+    argument_spec = dict(**UyuniAPIClient.argument_spec(),
+                         name=dict(type='str', required=True),
+                         )
+
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True
+    )
+
+    result = dict(
+                  changed=False,
+                  original_message='',
+                  msg=''
+                 )
+
+    if module.check_mode:
+         module.exit_json(**result)
+
+    api_instance = UyuniAPIClient(logging.ERROR,
+                                  module.params.get('uyuni_host'),
+                                  module.params.get('uyuni_user'),
+                                  module.params.get('uyuni_password'),
+                                  module.params.get('uyuni_port'),
+                                  verify=module.params.get('uyuni_verify_ssl'),
+                                  use_datetime=True,
+                                 )
+
+    systemID = api_instance.get_host_id(module.params.get('name'))
+    actionID = api_instance.execute_api_call("system.schedulePackageRefresh", systemID, 
+                                                                              datetime.now(timezone.utc)
+                                            )
+    resWait = api_instance.wait_for_action(actionID, systemID, interval=3)
+
+    result['changed'] = True
+    
+    if resWait[0]['failed_count'] > 0:
+        result['msg'] = "package_refresh failed"
+        result['original_message'] = resWait[0]['result_msg']
+        module.fail_json(**result)
+    else :
+        module.exit_json(**result)
+
+if __name__ == '__main__':
+    main()
