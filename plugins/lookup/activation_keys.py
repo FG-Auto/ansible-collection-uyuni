0206e200257c70597c865eba77f68daf462858b9
Klaus Hildebrandt <klaus.hildebrandt@edeka.de>
2025-07-25 10:03:16 +0200

    add lookup for activaton key(s)

---
+++ plugins/lookup/activation_keys.py
@@ -0,0 +1,50 @@
+# python 3 headers, required if submitting to Ansible
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+  name: file
+  author: Klaus Hildebrandt <klaus.hildebrandt@edeka.de>
+  version_added: "0.1"  # for collections, use the collection version, not the Ansible version
+  short_description: return activation_keys for given system
+  description:
+      - This lookup returns activation-keys for given system
+  options:
+    _terms:
+      description: system name
+      required: True
+    uyuni_host:
+      description:
+            - Sample option that could modify plugin behavior.
+            - This one can be set directly ``option1='x'`` or in ansible.cfg, but can also use vars or environment.
+      type: string
+    uyuni_user:
+
+    uyuni_password:
+
+  notes:
+    - if read in variable context, the file can be interpreted as YAML if the content is valid to the parser.
+    - this lookup does not understand globbing --- use the fileglob lookup instead.
+"""
+#from ansible.errors import AnsibleError, AnsibleParserError
+from ansible.plugins.lookup import LookupBase
+from ansible.utils.display import Display
+
+import logging
+
+from ..module_utils.uyuni import UyuniAPIClient
+
+display = Display()
+
+class LookupModule(LookupBase):
+
+    def run(self, terms, variables=None, **kwargs):
+      ret = []
+      #display.warning("type(terms): %s" % str(type(terms)))
+      api_instance = UyuniAPIClient(logging.ERROR, str(kwargs['uyuni_host']), str(kwargs['uyuni_user']), str(kwargs['uyuni_password']), use_datetime=True)
+      for term in terms:
+          #display.warning("term: %s" % term)
+          systemID = api_instance.get_host_id(term)
+          ret.append(api_instance.execute_api_call('system.listActivationKeys', systemID))
+
+      return ret
