3d3eb68b3960b85d25d4448d1697544a552dd790
Klaus Hildebrandt <klaus.hildebrandt@edeka.de>
2026-01-06 14:18:09 +0100

    fix typos

--- plugins/lookup/system_channels.py
+++ plugins/lookup/system_channels.py
@@ -48,7 +48,7 @@ class LookupModule(LookupBase):
       #display.warning("type(terms): %s" % str(type(terms)))
       named_params = dict(use_datetime=True)
       if 'uyuni_verify_ssl' in kwargs:
-         named_params['verify'] = kwargs['uyuni_verify_ssl']
+         named_params['uyuni_verify_ssl'] = kwargs['uyuni_verify_ssl']
       api_instance = UyuniAPIClient(logging.ERROR, str(kwargs['uyuni_host']), str(kwargs['uyuni_user']), str(kwargs['uyuni_password']), **named_params)
       for term in terms:
           #display.warning("term: %s" % term)
